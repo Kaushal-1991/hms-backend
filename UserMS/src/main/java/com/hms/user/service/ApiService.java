fa7259c5e86853f38de3238d4e255abbc70ac83c
Kaushal Raj Singh <rajkaushal02@gmail.com>
2026-08-20 08:11:05 -0700

    update in userservice

--- UserMS/src/main/java/com/hms/user/service/ApiService.java
+++ UserMS/src/main/java/com/hms/user/service/ApiService.java
@@ -1,6 +1,7 @@
 package com.hms.user.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
@@ -11,19 +12,23 @@ import reactor.core.publisher.Mono;
 
 @Service
 public class ApiService {
+	
+	@Value("${profiles.url}")
+	private String ProfileUrl;
+	
 	@Autowired
 	private WebClient.Builder webClient;
 	
 	public Mono<Long> addProfile(UserDto userDto){
 		if(userDto.getRole().equals(Role.DOCTOR)) {
 			return webClient.build().post()
-					                .uri("http://localhost:8081/profile/doctor/add")
+					                .uri(ProfileUrl+"/profile/doctor/add")
 					                .bodyValue(userDto)
 					                .retrieve()
 					                .bodyToMono(Long.class);
 		}else if(userDto.getRole().equals(Role.PATIENT)) {
 			return webClient.build().post()
-					                .uri("http://localhost:8081/profile/patient/add")
+					                .uri(ProfileUrl+"/profile/patient/add")
 					                .bodyValue(userDto)
 					                .retrieve()
 					                .bodyToMono(Long.class);
